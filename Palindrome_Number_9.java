0f6af9f67ee7239ce511d963ee5c99ccb45ed702
shrutikabagdi <131595392+shrutikabagdi@users.noreply.github.com>
2026-01-01 17:30:30 +0530

    Add files via upload

---
+++ Palindrome_Number_9.java
@@ -0,0 +1,26 @@
+package Leetcode;
+
+public class Palindrome_Number_9 {
+
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        int n = x;
+        int revNum = 0;
+
+        while (n > 0) {
+            int d = n % 10;
+            revNum = revNum * 10 + d;
+            n = n / 10;
+        }
+        return revNum == x;
+    }
+
+    public static void main(String[] args) {
+        Palindrome_Number_9 obj = new Palindrome_Number_9();
+
+        int input = 121;   
+        System.out.println(obj.isPalindrome(input));
+    }
+}
