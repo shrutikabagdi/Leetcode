e2362a351fdc8796faf0bed719e40a4c48cd17e3
shrutikabagdi <131595392+shrutikabagdi@users.noreply.github.com>
2026-02-03 23:08:55 +0530

    Add files via upload

---
+++ Valid_Parentheses_20.java
@@ -0,0 +1,17 @@
+package Leetcode;
+
+import java.util.Stack;
+
+public class Valid_Parentheses_20 {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') st.push(')');
+            else if (c == '[') st.push(']');
+            else if (c == '{') st.push('}');
+            else if (st.isEmpty() || st.pop() != c) return false;
+        }
+        return st.isEmpty();
+    }
+}
