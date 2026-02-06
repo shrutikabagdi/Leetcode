1ebf122204a448ceb2e2a205faa381f04662f94a
shrutikabagdi <131595392+shrutikabagdi@users.noreply.github.com>
2026-02-06 12:31:42 +0530

    Add files via upload

---
+++ Min_Stack_155.java
@@ -0,0 +1,35 @@
+package Leetcode;
+
+import java.util.Stack;
+
+public class Min_Stack_155 {
+    class MinStack {
+        int min = Integer.MAX_VALUE;
+        Stack<Integer> stack = new Stack<>();
+
+        public MinStack() {
+
+        }
+
+        public void push(int val) {
+            if(val <= min) {
+                stack.push(min);
+                min = val;
+            }
+            stack.push(val);
+
+        }
+
+        public void pop() {
+            if(stack.pop() == min) min = stack.pop();
+        }
+
+        public int top() {
+            return stack.peek();
+        }
+
+        public int getMin() {
+            return min;
+        }
+    }
+}
