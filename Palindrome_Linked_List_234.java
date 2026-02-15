d402d3d149e629ed69b4b780b707651560a85e19
shrutikabagdi <131595392+shrutikabagdi@users.noreply.github.com>
2026-02-15 22:42:14 +0530

    Add files via upload

---
+++ Palindrome_Linked_List_234.java
@@ -0,0 +1,19 @@
+package Leetcode;
+
+import java.util.Stack;
+
+public class Palindrome_Linked_List_234 {
+    public boolean isPalindrome(ListNode head) {
+        Stack<Integer> stack = new Stack();
+        ListNode curr = head;
+        while(curr != null) {
+            stack.push(curr.val);
+            curr = curr.next;
+        }
+        curr = head;
+        while(curr != null && curr.val == stack.pop()) {
+            curr = curr.next;
+        }
+        return curr == null;
+    }
+}
